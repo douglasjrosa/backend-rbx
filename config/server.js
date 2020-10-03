413670d2fa5e4840d0f50c4957d13067a3b3547d
douglasjrosa@gmail.com <douglasjrosa@gmail.com>
2020-10-03 20:30:10 -0300

    Project initialized

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e4a1aeb885a58cb687ffdebd10d1e95e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5fdbc516-0089-4d6f-b069-f746bee5074c'
+};
